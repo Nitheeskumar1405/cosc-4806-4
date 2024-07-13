370355e3ef850f3be92184f59c915c17a263642d
Nitheeskumar1405 <nitheeskumar.cs@gmail.com>
2024-07-13 02:04:50 +0000

    Created new table for reminders!

---
+++ app/views/reminders/index.php
@@ -0,0 +1,14 @@
+<?
+  php requrire_once 'app/views/templates/header.php'; ?>
+
+<div class="container">
+  <div class="page-header" id="banner">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Reminders</h1>
+      </div>
+    </div>
+  </div>
+
+
+  <?php requirere_once 'app/views/templates/footer.php'; ?>
